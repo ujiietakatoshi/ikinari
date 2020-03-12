bf9543f4a745d794aebda73b5eefee2b05fbcf76
ujiietakatoshi <fadeoutinu@gmail.com>
2020-03-12 23:07:58 +0900

    form

--- check.php
+++ check.php
@@ -6,16 +6,43 @@
 <body>
   <?php
   $nickname=$_POST['nickname'];
+  $email=$_POST['email'];
+  $goiken=$_POST['goiken'];
+
   if($nickname=='')
   {
-    print'ニックネームが入力されていません。';
+    print'ニックネームが入力されていません。<br/>';
   }
   else
   { 
     print'ようこそ';
     print $nickname;
     print'様';
+    print'<br/>';
   }
+
+  if($email=='')
+  {
+    print'emailが入力されていません。<br/>';
+  }
+  else
+  { 
+    print'メールアドレス:';
+    print $email;
+    print'<br/>';
+  }
+
+  if($goiken=='')
+  {
+    print'ご意見が入力されていません。<br/>';
+  }
+  else
+  { 
+    print'ご意見[';
+    print $goiken;
+    print']<br/>';
+  }
+
   ?>
   <!-- 画面の表示せよというprint命令 -->
 </body>
