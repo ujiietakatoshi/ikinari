f71c2f82777958b62a2eb2e20c44c029d0f37578
ujiietakatoshi <fadeoutinu@gmail.com>
2020-03-14 09:03:21 +0900

    commit2

--- thanks.php
+++ thanks.php
@@ -21,6 +21,13 @@
   print']<br/>';
   print $email;
   print'にメールを送りましたのでご確認ください。';
+  
+  $mail_sub= 'アンケートを受け付けました。';
+  $mail_body=$nickname."様へ\nアンケートご協力ありがとうございました。";
+  $mail_body=html_entity_decode($mail_body,ENT_QUOTES,"UTF-8");
+  $mail_head= 'Form:xxx@xxx.co.jp';
+  mb_internal_encoding("UTF-8");
+  mb_SEND_mail($email,$mail_sub,$mail_body,$mail_head);
   ?>
 </body>
 </html>
